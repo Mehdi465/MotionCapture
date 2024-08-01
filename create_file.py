b88923c25f6874c195251dc00bfdb9b73d1febdc
MehdiFellahi <mehdi.fellahi06@gmail.com>
2024-08-01 21:32:43 +0200

    wip

--- create_file.py
+++ create_file.py
@@ -1,8 +1,9 @@
+## Create a file with int from 1 to n_max, it was the select the ritgh dots for the right part.
+
 def create_file(n_max:int):
     with open("list_point.txt","w+") as list_file:
         for k in range(1,n_max+1):
             list_file.write(f"{k}\n")
 
-        
 if __name__ == "__main__":
     create_file(468)
